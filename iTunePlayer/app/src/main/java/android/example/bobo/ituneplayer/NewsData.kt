ccf94f4228b25cf26d5231cd978f378b2dd41dcb
bobo870214 <Bobocu870214>
2023-03-07 23:52:45 +0800

    HW1

---
+++ iTunePlayer/app/src/main/java/android/example/bobo/ituneplayer/NewsData.kt
@@ -0,0 +1,5 @@
+package android.example.bobo.ituneplayer
+
+class NewsData(val title: String ="") {
+
+}
---
+++ iTunePlayer2/app/src/main/java/android/example/bobo/ituneplayer2/SongData.kt
@@ -0,0 +1,5 @@
+package android.example.bobo.ituneplayer2
+
+class SongData(val title: String ="") {
+
+}

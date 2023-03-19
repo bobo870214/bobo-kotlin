aab28e528b235acedb695bf5a58861cfb192582b
bobo870214 <Bobocu870214>
2023-03-19 17:43:06 +0800

    Lab2-2

--- ColorMyViews/app/src/main/java/android/example/bobo/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/android/example/bobo/colormyviews/MainActivity.kt
@@ -1,10 +1,11 @@
 package android.example.bobo.colormyviews
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +15,6 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
-    private fun makeColored(view: View) {
-        when (view.id) {
-
-            // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
-
-        }
-
-
-    }
 
     private fun setListeners() {
         val boxOneText = findViewById<TextView>(R.id.box_one_text)
@@ -38,22 +24,43 @@ class MainActivity : AppCompatActivity() {
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
+
+        }
+
     }
 
 
-}
+    private fun makeColored(view: View) {
+        when (view.id) {
 
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.ic_launcher_background)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
 
-
-
+//            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+//            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+//            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
 
 }
 
